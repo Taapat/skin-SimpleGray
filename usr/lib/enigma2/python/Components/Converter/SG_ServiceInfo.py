8872e04a21cf3974e94054bdeb95100f606a2d54
Taapat <taapat@gmail.com>
2023-11-19 22:47:43 +0200

    Use own converter for service info in InfoBar
    Creates a converter that displays dolby, teletext, subtitiles and resolution in one string.

---
+++ usr/lib/enigma2/python/Components/Converter/SG_ServiceInfo.py
@@ -0,0 +1,47 @@
+from enigma import iServiceInformation, iPlayableService
+
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+
+
+class SG_ServiceInfo(Converter):
+	def __init__(self, type):
+		Converter.__init__(self, type)
+
+	@staticmethod
+	def add_str(ret, text):
+		return "%s %s" % (ret, text) if ret else text
+
+	@cached
+	def get_text(self):
+		service = self.source.service
+		info = service and service.info()
+		if not info:
+			return ""
+		audio = service.audioTracks()
+		ret = ""
+		if audio:
+			for i in range(audio.getNumberOfTracks()):
+				description = audio.getTrackInfo(i).getDescription()
+				if description and description.split()[0] in ("AC4", "AAC+", "AC3", "AC3+", "Dolby", "DTS", "DTS-HD", "HE-AAC", "WMA"):
+					ret += "DOLBY"
+		if info.getInfo(iServiceInformation.sTXTPID) != -1:
+			ret = self.add_str(ret, _("TEXT"))
+		if service.subtitle().getSubtitleList():
+			ret = self.add_str(ret, "SUB")
+		video_height = info.getInfo(iServiceInformation.sVideoHeight)
+		if video_height > 0:
+			if video_height < 720:
+				ret = self.add_str(ret, "SD")
+			elif video_height >= 1500:
+				ret = self.add_str(ret, "4K")
+			else:
+				ret = self.add_str(ret, "HD")
+			ret = self.add_str(ret, "%sx%s" % (str(info.getInfo(iServiceInformation.sVideoWidth)), str(video_height)))
+		return ret
+
+	text = property(get_text)
+
+	def changed(self, what):
+		if what[0] == self.CHANGED_SPECIFIC and what[1] in (iPlayableService.evUpdatedInfo, iPlayableService.evVideoSizeChanged):
+			Converter.changed(self, what)
